1791d88764d1442b7b43ab577cb2dc4e610a7d25
Denis.Biryuchevskiy <DenisBiryuchevskiy@gmail.com>
2023-02-18 18:14:28 +0300

    Задача по нахождению второй цифры в числе

---
+++ Lesson_2/Task_10/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int a = num / 10 % 10 ; //861/10=86%10=8
+Console.WriteLine($"Вторая цифра числа: {num} -> {a}");             //Код работает!
