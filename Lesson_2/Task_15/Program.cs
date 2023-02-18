60b9a7f55fa011b94b0ce289711900ec45976c72
Denis.Biryuchevskiy <DenisBiryuchevskiy@gmail.com>
2023-02-18 18:16:56 +0300

    Задача определяющая день недели

---
+++ Lesson_2/Task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.Clear();
+Console.WriteLine("Введите число от 1 до 7:");
+int num = int.Parse((Console.ReadLine())!);
+
+    if (num > 5)       
+    {
+        Console.WriteLine($"{num} -> ВЫХОДНОЙ");
+    }
+    else
+    {
+        Console.WriteLine($"{num} ->Go to WORK");}    // Код работает !
