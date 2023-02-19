f2f11c3d8a2d02412a1eec4d25201448c3c57595
Denis.Biryuchevskiy <DenisBiryuchevskiy@gmail.com>
2023-02-19 16:10:09 +0300

    Решение через индекс не получилось

--- Lesson1/Task_8/Program.cs
+++ Lesson1/Task_8/Program.cs
@@ -1,5 +1,5 @@
 ﻿ Console.WriteLine("Введите длину: ");
-int n = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine()!);
 for (int i = 1; i <= n; i++)
     if (i % 2 == 0)
         Console.WriteLine(i);
---
+++ Lesson_2/Task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿ 
+Console.Clear();
+Console.Write("Введите любое число: ");
+int a = Int32.Parse((Console.ReadLine())!);
+if(a <= 99){
+    Console.WriteLine("Число меньше 100");
+    }else{
+        while(a>1000){
+            a/=10; //a=a/10
+    }
+    Console.WriteLine(a%10);
+}
+/*
+    Index index1 = 2;
+   
+    Console.WriteLine($"{num} {index1}");
+
+
+Console.WriteLine("Введите любое число: ");
+    int IndexOf(int [] array, int value)
+    Index index2 = 2;
+    Console.WriteLine(numbers[index2]);
+*/
