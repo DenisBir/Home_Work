847b885799e8dc5f6fc74f4e6f4c75261301a47f
Denis.Biryuchevskiy <DenisBiryuchevskiy@gmail.com>
2023-02-20 08:05:30 +0300

    Решения модефикация

---
+++ Lesson_3/Task_19/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write("Введите любой номер для проверки на ПАЛИНДРОМ : ");
+int number = int.Parse(Console.ReadLine()!);
+int remineder, sum = 0;
+int temp = number;
+    while (number > 0)
+    {
+    remineder = number % 10;
+
+    sum = (sum * 10) + remineder;
+
+    number = number / 10; 
+    }
+        if (temp == sum)
+        {
+            Console.WriteLine($"Число {temp} ПАЛИНДРОМ");
+        }
+        else
+        {
+            Console.WriteLine($"Число {temp} не ПАЛИНДРОМ");
+        }
+    
